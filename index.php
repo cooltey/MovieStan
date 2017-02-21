84367c4a1d975698cb983986f83b34cc263d99b1
cooltey <coolteygame@gmail.com>
2017-02-21 12:30:00 -0800

    update index page

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-Welcome to MovieStan
 
-<?php
-	phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MovieStan</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container index-animation">
+      <div class="row">
+        <div class="col-sm-12" style="margin-top: 20%; text-align: center;">
+        	<img src="./images/brand.png" class="img-responsive" style="margin: 0 auto;">
+        	<br>
+        	Coming Soon...
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery-3.1.1.min.js"></script>
+    <script>window.jQuery || document.write('<script src="./ping/js/jquery-3.1.1.min.js"><\/script>')</script>
+    <script src="./js/bootstrap.min.js"></script>
+  </body>
+</html>
